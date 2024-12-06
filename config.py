0436879782967cdc100a03d306c3e9a4b491c77c
Paulo Fehlauer <paulofeh@gmail.com>
2024-12-06 09:45:04 -0300

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ CAPACITIES_BASE_URL = 'https://api.capacities.io'
 
 # Processing configuration
 ARTICLES_PER_RUN = 5
-ARTICLES_UPDATED_AFTER = "2024-11-01"
+ARTICLES_UPDATED_AFTER = "2024-12-05T00:00:00+00:00"
 DEFAULT_TAGS = ['a-processar', 'readwise']
 
 # File paths
